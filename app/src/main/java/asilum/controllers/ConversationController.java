c53077fdbab701ac7077907091cbcefa2958a2d9
Maxime Girma <mgi@ucar.fr>
2019-10-16 12:16:00 +0200

    cors test

--- app/src/main/java/asilum/controllers/ConversationController.java
+++ app/src/main/java/asilum/controllers/ConversationController.java
@@ -5,10 +5,8 @@ import asilum.DTO.ConversationDTO;
 import asilum.services.ConversationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.ArrayList;
 import java.util.List;
