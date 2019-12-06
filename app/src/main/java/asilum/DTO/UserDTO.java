f12c2bcff56f9db5d63879dc2e6b0544ea4a1857
tuyetrinhvo <tuyetrinhvo@gmail.com>
2019-12-06 12:07:58 +0100

    add proprities to UserDTO

--- app/src/main/java/asilum/DTO/UserDTO.java
+++ app/src/main/java/asilum/DTO/UserDTO.java
@@ -6,17 +6,25 @@ public class UserDTO {
     private String username;
     private String photoUri;
     private int id;
+    private String gender;
+    private Integer age;
+    private String country;
+    private String city;
 
     public UserDTO(User user) {
         this.id = user.getId();
         this.username = user.getUsername().getUsername();
         this.photoUri = user.getPhotoUri();
+        this.gender = user.getGender();
+        this.age = user.getAge();
+        this.country = user.getCountry();
+        this.city = user.getCity();
+
     }
 
     public String getUsername() {
         return username;
     }
-
 
     public void setUsername(String username) {
         this.username = username;
@@ -24,5 +32,21 @@ public class UserDTO {
 
     public String getPhotoUri() {
         return photoUri;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
     }
 }
